886b2d9628762c4aaa6c8735465b09c710f0b30e
FalinCat <igor.vanteev@gmail.com>
2018-12-29 09:33:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/forecast/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.forecast.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.forecast.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
